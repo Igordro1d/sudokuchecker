0a3983cd1c5fdb97e8039608503435cb89813d8f
Igordro1d <igormoreiracastro0@gmail.com>
2025-01-27 12:47:20 +1300

    renamed variables, added app.py flask server to main

--- main.py
+++ main.py
@@ -1,35 +1,13 @@
-from networkx.generators import sudoku
 import random
+from flask import Flask, render_template, request, jsonify
 
-# sudoku_board = [
-#     [5, 3, 4, 6, 7, 8, 9, 1, 2],
-#     [6, 7, 2, 1, 9, 5, 3, 4, 8],
-#     [1, 9, 8, 3, 4, 2, 5, 6, 7],
-#     [8, 5, 9, 7, 6, 1, 4, 2, 3],
-#     [4, 2, 6, 8, 5, 3, 7, 9, 1],
-#     [7, 1, 3, 9, 2, 4, 8, 5, 6],
-#     [9, 6, 1, 5, 3, 7, 2, 8, 4],
-#     [2, 8, 7, 4, 1, 9, 6, 3, 5],
-#     [3, 4, 5, 2, 8, 6, 1, 7, 9]
-# ]
-# sudoku_board2 = [
-#     [0, 3, 4, 6, 7, 8, 9, 1, 2],
-#     [6, 0, 2, 1, 9, 5, 3, 4, 8],
-#     [1, 9, 0, 3, 4, 2, 5, 6, 7],
-#     [8, 5, 9, 0, 6, 1, 4, 2, 3],
-#     [4, 2, 6, 8, 0, 3, 7, 9, 1],
-#     [7, 1, 3, 9, 2, 0, 8, 5, 6],
-#     [9, 6, 1, 5, 3, 7, 0, 8, 4],
-#     [2, 8, 7, 4, 1, 9, 6, 3, 5],
-#     [3, 4, 5, 2, 8, 6, 1, 7, 9]
-# ]
+app = Flask(__name__)
 
-def rowchecker(row: list):
+def checkIndividualRow(row: list):
     filter_row = [num for num in row if num !=0]
-    if len(filter_row) == len(set(filter_row)):
-        return True
+    return len(filter_row) == len(set(filter_row))
 
-def colchecker(board: list):
+def checkIndividualCol(board: list):
     answer = []
     for r in range(len(board)):
         block = []
@@ -38,7 +16,12 @@ def colchecker(board: list):
         answer.append(block)
     return answer
 
-def divideblocks(board: list):
+def divideBlock(board: list):
+    """
+    Divides the 9x9 board into 3x3 boards
+    :param board:
+    :return:
+    """
     answer = []
     for r in range(3):
         for c in range(3):
@@ -49,43 +32,48 @@ def divideblocks(board: list):
             answer.append(block)
     return answer
 
-def sudokucheck(board: list):
-    # for r in board:
-    #     if 0 in r:
-    #         return False
-    #checks each item in board to see if true
-    if not all(rowchecker(r) for r in board):
+def validSudokuBoard(board: list):
+    if not all(checkIndividualRow(r) for r in board):
         return False
-    if not all(rowchecker(r) for r in divideblocks(board)):
+    if not all(checkIndividualRow(r) for r in divideBlock(board)):
         return False
-    if not all(rowchecker(r) for r in colchecker(board)):
+    if not all(checkIndividualRow(r) for r in checkIndividualCol(board)):
         return False
     return True
 
-def emptycell(board: list):
-    emptycells = []
+def findEmptyCells(board: list):
+    """
+    Finds empty cells (0) in a board and returns the coordinates of it
+    :param board:
+    :return:
+    """
+    emptyCellCoordinates = []
     for r in range(len(board)):
         for c in range(len(board)):
             if board[r][c] == 0:
-                emptycells.append([r, c])
-    return emptycells
+                emptyCellCoordinates.append([r, c])
+    return emptyCellCoordinates
 
 def sudokusolver(board: list):
-    emptycells = emptycell(board)
-    if len(emptycells) == 0:
+    coordinatesOfEmptyCells = findEmptyCells(board)
+    if len(coordinatesOfEmptyCells) == 0:
         return board
-    cell = emptycells[0]
+    cell = coordinatesOfEmptyCells[0]
     r = cell[0]
     c = cell[1]
     for num in range(1,10):
         board[r][c] = num
-        if sudokucheck(board):
+        if validSudokuBoard(board):
             result = sudokusolver(board)
             if result is not None:
                 return result
         board[r][c] = 0
 
-def makeemptyboard():
+def createNewBoard():
+    """
+    adds 0's to each square in the board
+    :return:
+    """
     board = []
     for i in range(9):
         block = []
@@ -94,56 +82,67 @@ def makeemptyboard():
         board.append(block)
     return board
 
-def generateboard(board: list):
-    emptycells = emptycell(board)
-    if len(emptycells) == 0:
+def generateNewBoard(board: list):
+    """
+    creates a new board for a new game of sudoku
+    :param board:
+    :return:
+    """
+    coordinatesOfEmptyCells = findEmptyCells(board)
+    if len(coordinatesOfEmptyCells) == 0:
         return board
-    cell = emptycells[0]
+    cell = coordinatesOfEmptyCells[0]
     r = cell[0]
     c = cell[1]
     num = list(range(1,10))
     random.shuffle(num)
     for i in num:
         board[r][c] = i
-        if sudokucheck(board):
-            result = generateboard(board)
+        if validSudokuBoard(board):
+            result = generateNewBoard(board)
             if result is not None:
                 return result
         board[r][c] = 0
 
-def puzzlegenerator(board: list, difficulty):
-    x=0
-    if difficulty.lower() == 'easy':
-        x = 20
-    elif difficulty.lower() == 'medium':
-        x = 35
-    elif difficulty.lower() == 'hard':
-        x = 40
-    for i in range(x):
+def generateNewPuzzle(board: list, difficulty):
+    """
+    creates the final board the user will see
+    :param board: new generated board
+    :param difficulty: difficulty the user wants to play at, removes more spaces depending on difficulty
+    :return:
+    """
+    difficultyDic = {
+        'easy': 20,
+        'medium': 35,
+        'hard': 40
+    }
+    for i in range(difficultyDic[difficulty.lower()]):
         r = random.randint(0,8)
         c = random.randint(0,8)
         board[r][c] = 0
     return board
 
+@app.route('/')
+def home():
+    return render_template('index.html')
 
+@app.route('/solve', methods=['POST'])
+def solve():
+    data = request.get_json()
+    board = data['board']
+    solved = sudokusolver(board)
+    return jsonify({'solved': solved})
 
+@app.route('/generate', methods=['POST'])
+def generate():
+    data = request.get_json()
+    difficulty = data.get('difficulty', 'medium')
+    board = createNewBoard()
+    filled = generateNewBoard(board)
+    puzzle = generateNewPuzzle(filled, difficulty)
+    return jsonify({'board': puzzle})
 
-# def generateboard():
-#     board=[]
-#     for i in range(9):
-#         block=[]
-#         for j in range(9):
-#             block.append(0)
-#         board.append(block)
-#     for i in range(30):
-#         r = random.randint(0,8)
-#         c = random.randint(0,8)
-#         if board[r][c] == 0:
-#             board[r][c] = random.randint(1,9)
-#             if not sudokucheck(board):
-#                 board[r][c] = 0
-#     if sudokucheck(board):
-#         return board
-#     else:
-#         return generateboard()
+if __name__ == '__main__':
+    app.run(debug=True)
+
 
